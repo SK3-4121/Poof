5799e61ea04f13507ebc543474466644665e6dfb
SK3#3160 <66770178+SK3-4121@users.noreply.github.com>
2022-08-28 00:12:45 -0700

    Update install.sh

--- md/install.sh
+++ md/install.sh
@@ -1,8 +1,19 @@
 echo Installing Poof!;
-echo NOTE: MAKE SURE YOUR ON ROOT;
+# echo NOTE: MAKE SURE YOUR ON ROOT;
 
-function Test-Administrator  
-{  
-    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
-    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)  
+ClearDirectory() {
+    rm -r C:/poof;
 }
+
+ClearDirectory;
+mkdir C:/poof;
+
+touch C:/poof/Settings.json;
+echo "{\"Silent_Mode\": true, \"Auto_Clear\": true}" > C:/poof/Settings.json;
+
+powershell -ExecutionPolicy Bypass -Command 'wget "https://github.com/SK3-4121/Poof/releases/download/Purple/poof.exe" -o C:\poof\poof.exe';
+
+set PATH=$PATH:~C:/poof
+
+echo Installed Poof!;
+read -t 5;
