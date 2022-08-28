f7425ce8ef38ed301496cceb3c3cb8d35465cf5f
SK3#3160 <66770178+SK3-4121@users.noreply.github.com>
2022-08-27 23:06:04 -0700

    Update install.sh

--- md/install.sh
+++ md/install.sh
@@ -1,1 +1,8 @@
-echo Test;
+echo Installing Poof!;
+echo NOTE: MAKE SURE YOUR ON ROOT;
+
+function Test-Administrator  
+{  
+    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)  
+}
